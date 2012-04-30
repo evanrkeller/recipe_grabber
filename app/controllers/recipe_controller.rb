f0e83bccb3b8019ea95d31882cb0d1e81ce73810
Evan Keller <ekeller@barefootproximity.com>
2012-04-30 00:12:30 -0500

    Update sample up to work with new website redesign

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -3,11 +3,11 @@ class RecipeController < ApplicationController
   require 'open-uri'
 
   def index
-    recipe_url = params[:recipe_url] || 'http://find.myrecipes.com/recipes/recipefinder.dyn?action=displayRecipe&recipe_id=1891929'
+    recipe_url = params[:recipe_url] || 'http://www.myrecipes.com/recipe/chicken-cashews-10000001891929/'
     doc = Hpricot(open(recipe_url.strip))
-    @title = doc/"//h1[@id=recipeTitle]"
-    @ingredients = doc/"//div[@id=ingredients]/ul/li"
-    @preparation = doc/"//div[@id=preparation]/*"
+    @title = doc/"//h1[@class=x4-headline]"
+    @ingredients = doc/"//div[@class=recipeDetails]/ul/li"
+    @steps = doc/"//div[@class=recipeDetails]/ol/li"
 
   end
 end
