0825069f264562656328d56ce945f71c54dcc7aa
Evan Keller <ekeller@barefootproximity.com>
2012-05-01 22:43:26 -0500

    Set tool up to retrieve multiple recipes

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -3,21 +3,27 @@ class RecipeController < ApplicationController
   require 'open-uri'
 
   def index
-    recipe_url = params[:recipe_url] || 'http://www.myrecipes.com/recipe/chicken-cashews-10000001891929/'
-    doc = Nokogiri::HTML(open(recipe_url.strip))
-    @title = doc.search("//meta[@name='recipe_name']/@content").to_s.strip
-    @head_note = doc.search("//meta[@property='og:description']/@content").to_s.strip
-    @image = doc.search("//meta[@property='og:image']/@content").to_s.strip
-    @yield = doc.search("//span[@itemprop='yield']/text()").to_s.strip
-    @prep_time = doc.search("//time[@itemprop='prepTime']/text()").to_s.strip
-    @cook_time = doc.search("//time[@itemprop='cookTime']/text()").to_s.strip
-    @total_time = doc.search("//time[@itemprop='totalTime']/text()").to_s.strip
-    @ingredients = doc.search("//div[@class='recipeDetails']/ul/li")
-    @steps = doc.search("//div[@class='recipeDetails']/ol/li")
-    @author = doc.search("//div[@class='byline']/p[@class='author']/text()")[0].to_s.strip.gsub(/,/, '')
-    @publication = doc.search("//div[@class='byline']/p[@class='author']/a//text()").to_s.strip
-    @published = doc.search("//div[@class='byline']/p[@class='author']/span[@itemprop='published']/text()").to_s.strip.capitalize
-    @nutritional_analysis = doc.search("//ul[@itemprop='nutrition']/li")
-
+    recipe_urls = params[:recipe_url].to_a.map(&:strip)
+    @recipes = [].tap do |recipes|
+      recipe_urls.each do |recipe_url|
+        doc = Nokogiri::HTML(open(recipe_url.strip))
+        recipe = {}
+        recipe[:title] = doc.search("//meta[@name='recipe_name']/@content").to_s.strip
+        recipe[:head_note] = doc.search("//meta[@property='og:description']/@content").to_s.strip
+        recipe[:image] = doc.search("//meta[@property='og:image']/@content").to_s.strip
+        recipe[:yield] = doc.search("//span[@itemprop='yield']/text()").to_s.strip
+        recipe[:prep_time] = doc.search("//time[@itemprop='prepTime']/text()").to_s.strip
+        recipe[:cook_time] = doc.search("//time[@itemprop='cookTime']/text()").to_s.strip
+        recipe[:total_time] = doc.search("//time[@itemprop='totalTime']/text()").to_s.strip
+        recipe[:ingredients] = doc.search("//div[@class='recipeDetails']/ul/li")
+        recipe[:steps] = doc.search("//div[@class='recipeDetails']/ol/li")
+        recipe[:author] = doc.search("//div[@class='byline']/p[@class='author']/text()")[0].to_s.strip.gsub(/,/, '')
+        recipe[:publication] = doc.search("//div[@class='byline']/p[@class='author']/a//text()").to_s.strip
+        recipe[:published] = doc.search("//div[@class='byline']/p[@class='author']/span[@itemprop='published']/text()").to_s.strip.capitalize
+        recipe[:nutritional_analysis] = doc.search("//ul[@itemprop='nutrition']/li")
+        recipes << recipe
+      end
+    end
   end
+
 end
